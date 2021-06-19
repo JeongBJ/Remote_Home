7bf0e7c3994dc048e9948cbaef6105ac3ea7a7ed
JeongBJ <85900947+JeongBJ@users.noreply.github.com>
2021-06-20 02:31:45 +0900

    0

---
+++ app/src/main/java/com/example/remote_home/Etc.kt
@@ -0,0 +1,7 @@
+package com.example.remote_home
+
+object API {
+    private const val WEATHER_URL : String = "http://apis.data.go.kr/1360000/VilageFcstInfoService/"
+    private const val WEATHER_KEY : String = "JHzJ3k6MD3cYuG9n7fWSEDABiqrV7J5gxbqs0%2FPPbOno5WzeXQGadJNhsNhTi4jNMkm6MgTpUkjPSE0L1yPy4A%3D%3D"
+
+}
---
+++ app/src/main/java/com/example/remote_home/Weather.kt
@@ -0,0 +1,5 @@
+package com.example.remote_home
+
+class Weather {
+
+}
